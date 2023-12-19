bc6e3dbcb36fc1bc7c6b969cdae0378349d0f26e
Nirob Hasan <nirob427417@gmail.com>
2023-12-19 15:11:55 +0600

    user crud operation

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,6 +3,7 @@ import { ReviewAndRatingRoutes } from '../modules/ReviewAndRating/ReviewAndRatin
 import { AuthRoutes } from '../modules/auth/auth.routes';
 import { CategoryRoutes } from '../modules/category/category.routes';
 import { TrailerRoutes } from '../modules/trailer/trailer.routes';
+import { UserRoutes } from '../modules/user/user.routes';
 import { MovieRoutes } from './../modules/movie/movie.routes';
 
 const router = express.Router();
@@ -12,6 +13,10 @@ const moduleRoutes = [
   {
     path: '/auth',
     route: AuthRoutes,
+  },
+  {
+    path: '/users',
+    route: UserRoutes,
   },
   {
     path: '/categories',
